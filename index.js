243da24148f225b4387e4e1149aa62a34784e341
valbuildr <150450259+valbuildr@users.noreply.github.com>
2026-06-15 23:09:55 -0500

    Improving random NWS alerts

--- index.js
+++ index.js
@@ -24,22 +24,148 @@ app.command("/starslack-nws-alert", async ({ ack, respond }) => {
 
         const alert = resp.data.features[Math.floor(Math.random() * resp.data.features.length)];
 
-        const id = alert.id;
+        const id = alert.properties.id;
         const event = alert.properties.event;
         const vtec = alert.properties.parameters.VTEC[0];
         const headline = alert.properties.headline;
-        const description = alert.properties.description;
-        const instruction = alert.properties.instruction;
-        const senderCode = vtec.split(".")[2];
-        const senderUrl = "https://weather.gov/" + senderCode.slice(1).toLowerCase();
-        const sender = alert.properties.senderName;
 
         const codeBlock = (txt) => { return "```" + txt + "```"; }
 
-        await respond({ text: `*<${id}|${event}> (\`${vtec}\`)*\nHeadline:\n${codeBlock(headline)}\nDescription:\n${codeBlock(description)}\nInstruction:\n${codeBlock(instruction)}\nIssued by <${senderUrl}|${sender}>. (${senderCode})` });
+        await respond({
+            blocks: [
+                {
+                    type: "card",
+                    title: {
+                        type: "mrkdwn",
+                        text: event,
+                        verbatim: true
+                    },
+                    subtitle: {
+                        type: "mrkdwn",
+                        text: `\`${vtec}\``,
+                        verbatim: true
+                    },
+                    body: {
+                        type: "mrkdwn",
+                        text: headline.length > 200 ? headline.slice(0, 197) + "..." : headline,
+                        verbatim: true
+                    },
+                    actions: [
+                        {
+                            type: "button",
+                            style: "primary",
+                            text: {
+                                type: "plain_text",
+                                text: "Learn more",
+                                emoji: false
+                            },
+                            action_id: "starslack-nws-alert-more",
+                            value: id
+                        }
+                    ]
+                }
+            ]
+        });
     } catch (err) {
         console.error(err);
         await respond({ text: "Failed to fetch an alert." });
+    }
+});
+
+app.action("starslack-nws-alert-more", async ({ ack, body, client, action }) => {
+    await ack();
+
+    const alertId = action.value;
+
+    try {
+        const alert = await axios.get(`https://api.weather.gov/alerts/${alertId}`);
+
+        const event = alert.data.properties.event;
+        const vtec = alert.data.properties.parameters.VTEC[0];
+        const headline = alert.data.properties.headline ?? "None";
+        const description = alert.data.properties.description ?? "None";
+        const instruction = alert.data.properties.instruction ?? "None";
+        const sender = alert.data.properties.senderName;
+        const senderCode = vtec.split(".")[2];
+        const senderUrl = "https://weather.gov/" + senderCode.slice(1).toLowerCase();
+
+        await client.views.open({
+            trigger_id: body.trigger_id,
+            view: {
+                type: "modal",
+                title: {
+                    type: "plain_text",
+                    text: event,
+                    emoji: true
+                },
+                close: {
+                    type: "plain_text",
+                    text: "Close",
+                    emoji: true
+                },
+                blocks: [
+                    {
+                        type: "alert",
+                        text: {
+                            type: "mrkdwn",
+                            text: "Starslack is *not* meant for use in life-threatening situations.\nIf you're in a life-threatening scenario, *please use <https://weather.gov|official sources> instead*.",
+                            verbatim: true
+                        },
+                        level: "warning"
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `*VTEC Code:*\n\`${vtec}\``
+                        }
+                    },
+                    {
+                        type: "divider"
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `*Headline:*\n\`\`\`${headline}\`\`\``
+                        }
+                    },
+                    {
+                        type: "divider"
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `*Description:*\n\`\`\`${description}\`\`\``
+                        }
+                    },
+                    {
+                        type: "divider"
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `*Instruction:*\n\`\`\`${instruction}\`\`\``
+                        }
+                    },
+                    {
+                        type: "divider"
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `Issued by <${senderUrl}|${sender}>. (${senderCode})\nView the full data <https://api.weather.gov/alerts/${alertId}|here>.`
+                        }
+                    }
+                ]
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        // TODO: respond?
     }
 });
 
